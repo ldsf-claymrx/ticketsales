c71d15715a240e4c4f6da181f58bdadf32744092
Ing. Lucio David <santiago.floresld@gmail.com>
2024-07-22 17:09:14 -0600

    TicketSales 1.0.1 - Backup Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         @vite(['resources/css/app.css','resources/js/app.js'])
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
         <title>Impacto Juvenil 2024</title>
     </head>
     <body>        
@@ -16,13 +13,13 @@
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="{{ asset('img/logo-oficial-transparente-blanco.png') }}" class="h-8" alt="CCA Logo Oficial">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">CCA</span>
+                    <span class="self-center text-2xl font-bold whitespace-nowrap text-white">CCA</span>
                 </a>
 
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Comprar boletos</button>
+                    <button type="button" class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center">Registrar mis boletos</button>
                     
-                    <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                    <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -46,37 +43,118 @@
 
         
 
-        <section class="bg-center bg-no-repeat bg-[url('http://imgfz.com/i/eAbzSjJ.jpeg')] bg-gray-500 bg-blend-multiply">
+        <section class="bg-cover bg-no-repeat bg-[url('http://imgfz.com/i/a4jgMh7.png')] bg-gray-700 bg-blend-multiply">
             <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Congreso Impacto Juvenil 2024</h1>
-                <p class="mb-8 text-lg font-quicksand text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-                    No pierdas la oportunidad de buscar más de DIOS, regístrate a este congreso, tendremos invitados especiales como: La Banda Conquistando Fronteras, Misael Barrera & Kharis Millán
+                <h1 class="mb-4 pt-9 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Congreso Impacto Juvenil 2024</h1>
+                <p class="mb-8 text-lg text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+                    ¡Registra tus boletos ahora!
                 </p>
                 
                 <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                    <a href="#" class="font-quicksand inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-900">
-                        Comprar boletos
+                    <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-900">
+                        Registrar mis boletos
                         <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
                     </a>
-                    <a href="#" class="font-quicksand inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                    <a href="#info" id="info" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
                         Más Información
                     </a>  
                 </div>
             </div>
         </section>
 
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+                <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Impacto Juvenil 2024</h2>
+                    <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+                        Este 22 & 23 de noviembre 2024, con sede en: Auditorio Municipal de Altamira, Tamaulipas. 
+                        Tendremos nuestro Congreso "Impacto Juvenil 2024", que estará dirigido a todos los jóvenes de la zona conurbada de 
+                        Altamira, Tampico y Madero. Y a todas las iglesias que deseen acompañarnos.
+
+                        <br><br> ¡Tendremos invitados especiales!
+                    </p>
+                </div> 
+                <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
+                    <div class="text-center text-gray-500 dark:text-gray-400">
+                        <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="{{ asset('img/conquistandofronteras.png') }}" style="object-fit: cover;" alt="Banda Conquistando Fronteras">
+                        <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Banda Conquistando Fronteras</h3>
+                        <p>Alabanza & Adoración</p>
+                        <ul class="flex justify-center mt-4 space-x-4">
+                            <li>
+                                <a href="https://www.facebook.com/MusicaICF" target="_blank" class="text-[#39569C] hover:text-[#0F2F7C]">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://x.com/BandaCF" target="_blank" class="text-[#00ACEE] hover:text-[#1084B0]">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/@BandaCFoficial" target="_blank" class="text-[#FC0F0F] hover:text-[#B40F0F]">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clip-rule="evenodd"/></svg>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
 
 
-        <section class="p-10 font-quicksand text-justify">
-            <h1 class="text-2xl font-bold">Impacto Juvenil 2024</h1>
-            <p class="pt-2 text-gray-500">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+                    <div class="text-center text-gray-500 dark:text-gray-400">
+                        <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="{{ asset('img/misael.png') }}" style="object-fit: cover;" alt="Banda Conquistando Fronteras">
+                        <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Misael Barrera & Kharis Millán</h3>
+                        <p>Portadores de la Palabra de DIOS</p>
+                        <ul class="flex justify-center mt-4 space-x-4">
+                            <li>
+                                <a href="https://www.facebook.com/MusicaICF" target="_blank" class="text-[#39569C] hover:text-[#0F2F7C]">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://x.com/BandaCF" target="_blank" class="text-[#00ACEE] hover:text-[#1084B0]">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/@BandaCFoficial" target="_blank" class="text-[#FC0F0F] hover:text-[#B40F0F]">
+                                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clip-rule="evenodd"/></svg>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>  
+            </div>
         </section>
-        
 
-
-
-
+        <footer class="p-4 bg-gray-800 md:p-8 lg:p-10">
+            <div class="mx-auto max-w-screen-xl text-center">
+                <a href="" class="flex justify-center items-center text-2xl font-semibold text-white">
+                    <img src="{{ asset('img/logo-oficial-transparente-blanco.png') }}" width="24px" class="mr-2" alt="CCA">
+                    Comunidad Cristiana Altamira 
+                </a>
+                <p class="my-6 text-gray-400">Es un gusto saludarte, te invitamos a visitar nuestra página web, para saber más de nosotros. DIOS te bendiga.</p>
+                <ul class="flex flex-wrap justify-center items-center mb-6 text-white">
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">CCA Oficial Web</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Preguntas Frecuentes</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Contáctanos</a>
+                    </li>
+                </ul>
+                <span class="text-sm font-bold text-gray-400 sm:text-center">
+                    Create by
+                    <a href="" class="inline-flex justify-center items-center hover:underline">
+                        DavidClayMRX
+                        <svg class="w-6 h-6 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd" d="M12 2c-.791 0-1.55.314-2.11.874l-.893.893a.985.985 0 0 1-.696.288H7.04A2.984 2.984 0 0 0 4.055 7.04v1.262a.986.986 0 0 1-.288.696l-.893.893a2.984 2.984 0 0 0 0 4.22l.893.893a.985.985 0 0 1 .288.696v1.262a2.984 2.984 0 0 0 2.984 2.984h1.262c.261 0 .512.104.696.288l.893.893a2.984 2.984 0 0 0 4.22 0l.893-.893a.985.985 0 0 1 .696-.288h1.262a2.984 2.984 0 0 0 2.984-2.984V15.7c0-.261.104-.512.288-.696l.893-.893a2.984 2.984 0 0 0 0-4.22l-.893-.893a.985.985 0 0 1-.288-.696V7.04a2.984 2.984 0 0 0-2.984-2.984h-1.262a.985.985 0 0 1-.696-.288l-.893-.893A2.984 2.984 0 0 0 12 2Zm3.683 7.73a1 1 0 1 0-1.414-1.413l-4.253 4.253-1.277-1.277a1 1 0 0 0-1.415 1.414l1.985 1.984a1 1 0 0 0 1.414 0l4.96-4.96Z" clip-rule="evenodd"/>
+                        </svg>                          
+                    </a>
+                </span>
+            </div>
+        </footer>
     </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,41 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      quicksand: ['Quicksand', 'sans-serif'],
+      'body': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+      ],
+
+      'sans': [
+        'Inter', 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'
+      ]
     },
   },
   plugins: [
